2039d0d21e8e4ccaa0ffd02bc8a5b32d6e6ff87f
Alonso Sagrero <alonsosagrerogranados@gmail.com>
2025-04-14 19:57:20 -0600

    Se agregan los calculos del menu

--- src/Desafiro.java
+++ src/Desafiro.java
@@ -1,9 +1,15 @@
+import java.util.Scanner;
+
 public class Desafiro {
 
     public static void main(String[] args) {
         String nombre = "Tony Stark";
         String tipoDeCuenta = "Corriente";
         double saldo = 1599.99;
+        int opcion = 0;
+        double retiro = 0;
+        double ingreso = 0;
+        Scanner teclado = new Scanner(System.in);
 
         System.out.println("***********************");
         System.out.println("\nNombre del cliente: " + nombre);
@@ -13,14 +19,36 @@ public class Desafiro {
 
         String menu = """
                 *** Escriba el número de la opción deseada ***
-                1. Consultar saldo.
-                2. Retirar.
-                3. Depositar.
-                9. Salir.
+                1 - Consultar saldo.
+                2 - Retirar.
+                3 - Depositar.
+                9 - Salir.
                 """;
 
-        while (opcion != 9){
 
+        while (opcion != 9){
+            System.out.println(menu);
+            opcion = teclado.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    System.out.println("Su saldo es de: " + saldo);
+                    break;
+                case 2:
+                    System.out.println("Ingrese la cantidad a retirar: ");
+                    retiro = teclado.nextDouble();
+                    saldo -= retiro;
+                    System.out.println("Su saldo disponible es: " + saldo);
+                    break;
+                case 3:
+                    System.out.println("Ingrese la cantidad a depositar: ");
+                    ingreso = teclado.nextDouble();
+                    saldo += retiro;
+                System.out.println("Su saldo disponible es: " + saldo);
+                default:
+                    System.out.println("Opción inválida");
+                    break;
+            }
         }
     }
 }
